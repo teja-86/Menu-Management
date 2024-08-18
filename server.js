9a7cf50d0e10b454aec79bb088bbc048ab250d09
Ranga Dharma Teja Kuntumalla <134760904+teja-86@users.noreply.github.com>
2024-08-18 16:34:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ app.use('/api/items', itemRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/categories/:categoryId/subcategories', subCategoryRoutes);
 app.use('/api/subcategories/:subCategoryId/items', itemRoutes);
+app.get('/', (req, res) => res.send('Welcome to the Menu Management'));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
